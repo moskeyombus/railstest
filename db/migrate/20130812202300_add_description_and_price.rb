e3a39d3d3e57002c0c31dd122e41e0a8fc574c15
MoskeyOmbus <john.wnek@gmail.com>
2013-08-12 16:51:26 -0400

    Added description and price to product

---
+++ db/migrate/20130812202300_add_description_and_price.rb
@@ -0,0 +1,4 @@
+class AddDescriptionAndPrice < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130812191328) do
+ActiveRecord::Schema.define(version: 20130812203057) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
+    t.decimal  "price",       precision: 8, scale: 2
   end
 
   create_table "users", force: true do |t|
